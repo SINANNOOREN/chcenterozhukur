3f74d7e0181c97597469fa3bf06f6685548428bd
wrixcode <wrixcode@gmail.com>
2025-06-03 20:43:56 +0530

    ss

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -5,7 +5,7 @@ import Hero from '@/components/Hero';
 import About from '@/components/About';
 import Services from '@/components/Services';
 import Doctors from '@/components/Doctors';
-import NewsSection from '@/components/NewsSection';
+
 import Contact from '@/components/Contact';
 import Footer from '@/components/Footer';
 
